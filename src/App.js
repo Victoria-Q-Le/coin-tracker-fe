178b5f82ca9a5815fc06032bcd65688f52b9bace
Victoria Le <leqvictoria@gmail.com>
2022-07-27 23:17:27 -0400

    outes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+
+import Header from './Components/Header';
+
+import HomePage from'./Pages/HomePage';
+import CoinPage from'./Pages/CoinPage';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Route path='/' component={HomePage} exact />
+        <Route path='/coins/:id' component={CoinPage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
