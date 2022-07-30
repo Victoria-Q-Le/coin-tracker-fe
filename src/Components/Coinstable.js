2acda9f11d1e7ec43fb321283845cc1512184f3b
Victoria Le <leqvictoria@gmail.com>
2022-07-29 20:24:38 -0400

    Working on coin table

--- src/Components/Coinstable.js
+++ src/Components/Coinstable.js
@@ -1,18 +1,44 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { CoinList } from '../config/api'
+import { CoinState } from '../CoinContext'
+import { Container, createTheme, ThemeProvider, Typography } from '@material-ui/core'
 
 const Coinstable = () => {
   const [coins, setCoins] = useState([])
   const [loading, setLoading] = useState(false)
+  const {currency} = CoinState()
 
   const fetchCoins = async() => {
-    const {data} = await axios.get(CoinList)
+    setLoading(true)
+    const {data} = await axios.get(CoinList(currency)); //destructuring data 
+    setCoins(data)
+    setLoading(false)
   }
+  useEffect(() => {
+    fetchCoins()
+  },[currency])
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff"
+      },
+      type: "dark",
+    }
+  })
+
   return (
-    <div>
-      Coins Table 
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{textAlign: "center"}}>
+        <Typography
+          variant='h4'
+          style = {{margin: 18, fontFamily: "Montserrat"}}
+        >
+          
+        </Typography>
+      </Container>
+    </ThemeProvider>
   )
 }
 
