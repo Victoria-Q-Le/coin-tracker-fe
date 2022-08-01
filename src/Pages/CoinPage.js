8e09cea79f568166ff2e5cecc779fc6b7c199bf7
Victoria Le <leqvictoria@gmail.com>
2022-08-01 17:20:49 -0400

    succesfully making call to obtain single coin info

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,10 +1,9 @@
-import { makeStyles } from '@material-ui/styles'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { CoinState } from '../CoinContext'
-import CoinInfo from '../Components/CoinInfo'
 import { SingleCoin } from '../config/api'
+import {CoinState} from '../CoinContext'
+
 
 const CoinPage = () => {
   const {id} = useParams()
@@ -15,26 +14,14 @@ const CoinPage = () => {
     const {data} = await axios.get(SingleCoin(id))
     setCoin(data)
   }
-  console.log(coin);
-
   useEffect(() => {
     fetchCoin()
-  }, [currency])
-
-  const useStyles = makeStyles({
-
-  })
-
-  const classes = useStyles()
+  },[])
+  console.log(coin);
+  
   return (
-    <div className={classes.container}>
-      {/* Side Bar */}
-      <div className= {classes.sidebar}>
-
-      </div>
-
-      {/* Chart */}
-      <CoinInfo />
+    <div>
+      Coin Page 
     </div>
   )
 }
