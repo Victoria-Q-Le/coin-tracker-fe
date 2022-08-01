af76214ae6941c8e9842ac5e8b18e48f4831acfd
Victoria Le <leqvictoria@gmail.com>
2022-08-01 16:01:14 -0400

    constructing coin page

--- src/Components/Coinstable.js
+++ src/Components/Coinstable.js
@@ -102,7 +102,7 @@ const Coinstable = () => {
                     const profit = row.price_change_percentage_24h > 0;
                     return (
                       <TableRow
-                        onClick = {() => history.pushState(`/coins/${row.id}`)}
+                        onClick = {() => history.push(`/coins/${row.id}`)}
                         className = {classes.row}
                         key = {row.name}
                       >
--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { CoinState } from '../CoinContext'
+import CoinInfo from '../Components/CoinInfo'
+import { SingleCoin } from '../config/api'
 
 const CoinPage = () => {
+  const {id} = useParams()
+  const [coin, setCoin] = useState()
+  const {currency, symbol} = CoinState()
+
+  const fetchCoin = async() => {
+    const {data} = await axios.get(SingleCoin(id))
+    setCoin(data)
+  }
+  console.log(coin);
+
+  useEffect(() => {
+    fetchCoin()
+  }, [currency])
+
+  const useStyles = makeStyles({
+
+  })
+
+  const classes = useStyles()
   return (
-    <div>
-      CoinPage
+    <div className={classes.container}>
+      {/* Side Bar */}
+      <div className= {classes.sidebar}>
+
+      </div>
+
+      {/* Chart */}
+      <CoinInfo />
     </div>
   )
 }
