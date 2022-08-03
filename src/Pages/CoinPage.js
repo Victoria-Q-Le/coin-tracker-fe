64def615a5f76ad0f71c8049b0f6b07faeff7ebb
Victoria Le <leqvictoria@gmail.com>
2022-08-02 20:30:09 -0400

    Current Price section

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -44,6 +44,14 @@ const CoinPage = () => {
       fontWeight: "bold",
       marginBottom: 20,
       fontFamily: "Montserrat"
+    },
+    description: {
+      width: "100%",
+      fontFamily: "Montserrat",
+      padding:25,
+      paddingBottom: 15,
+      paddingTop: 0,
+      textAlign: "justify"
     }
   }))
 
@@ -64,10 +72,45 @@ const CoinPage = () => {
 
         <Typography variant='h3' className={classes.heading}> {coin?.name} </Typography>
 
-        <Typography variant='subtitle1'> 
+        <Typography variant='subtitle1' className={classes.description}> 
           <div dangerouslySetInnerHTML={{__html: coin?.description.en}}></div>
           {/* {coin?.description.en} */}
         </Typography>
+
+        <div className={classes.marketData}>
+          <span style={{display: 'flex'}}>
+            <Typography variant='h5' className={classes.heading}>Rank: </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant='h5'
+              style={{fontFamily: "Montserrat"}}
+            >
+              {coin?.market_cap_rank}
+            </Typography>
+          </span>
+
+          <span style={{display: 'flex'}}>
+            <Typography variant='h5' className={classes.heading}>Current Price: </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant='h5'
+              style={{fontFamily: "Montserrat"}}
+            >
+             {symbol} {" "}
+            </Typography>
+          </span>
+
+          {/* <span style={{display: 'flex'}}>
+            <Typography variant='h5' className={classes.heading}>Current Price: </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant='h5'
+              style={{fontFamily: "Montserrat"}}
+            >
+              {symbol} {" "} 
+            </Typography>
+          </span> */}
+        </div>
 
       {/* Chart */}
       <CoinInfo coin = {coin}/>
